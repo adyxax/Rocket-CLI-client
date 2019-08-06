2adde9fc42145c0ca5d7aada0bdac336debf5487
Julien Dessaux <julien.dessaux@adyxax.org>
2019-08-06 11:19:31 +0200

    Cosmetics

--- restapi/im.c
+++ restapi/im.c
@@ -7,23 +7,23 @@
 #include "common/http.h"
 #include "im.h"
 
-#define LOGIN_ARG_PRE "{ \"username\": \""
-#define LOGIN_ARG_POST "\" }"
+#define USERNAME_ARG_PRE "{ \"username\": \""
+#define USERNAME_ARG_POST "\" }"
 
 char // returns 0 if ok, greater than 0 otherwise
 restapi_im_close(const char* username)
 {
     char ret = 0;
 
-    size_t pre_len = strlen(LOGIN_ARG_PRE);
+    size_t pre_len = strlen(USERNAME_ARG_PRE);
     size_t user_len = strlen(username);
-    size_t post_len = strlen(LOGIN_ARG_POST);
-    char* login_args = malloc(pre_len + user_len + post_len + 1);
-    strcpy(login_args, LOGIN_ARG_PRE);
-    strcpy(login_args + pre_len, username);
-    strcpy(login_args + pre_len + user_len, LOGIN_ARG_POST);
-    const char* buffer = http_post("/api/v1/im.close", login_args);
-    free(login_args);
+    size_t post_len = strlen(USERNAME_ARG_POST);
+    char* username_arg = malloc(pre_len + user_len + post_len + 1);
+    strcpy(username_arg, USERNAME_ARG_PRE);
+    strcpy(username_arg + pre_len, username);
+    strcpy(username_arg + pre_len + user_len, USERNAME_ARG_POST);
+    const char* buffer = http_post("/api/v1/im.close", username_arg);
+    free(username_arg);
 
     if (buffer == NULL) {
         fprintf(stderr, "Error while im_close, http post didn't return any data.\n");
